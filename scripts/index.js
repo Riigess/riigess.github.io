48613568f1b3e0470d3eea444818fa768f9933d1
Tree <56428356+Riigess@users.noreply.github.com>
2024-07-15 21:08:06 -0700

    Updating info-and-development

--- scripts/index.js
+++ scripts/index.js
@@ -6,5 +6,11 @@ function changeImage() {
         document.getElementById('btnSwitch').src = "https://uxwing.com/wp-content/themes/uxwing/download/nature-and-environment/moon-icon.png";
         document.documentElement.setAttribute('data-bs-theme', 'dark');
     }
-  }
-  
+}
+
+function adjustNavbarWidth() {
+    let navbar = document.getElementsByClassName("roundedbottom")[0];
+    console.log(navbar.offsetWidth);
+}
+
+adjustNavbarWidth();
